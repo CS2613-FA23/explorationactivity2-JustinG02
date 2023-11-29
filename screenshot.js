845e3cdbc65ee033a99947cfa90b01e70c01121f
JustinG02 <jgalla13@unb.ca>
2023-11-28 21:20:09 -0400

    Changes to example program

--- screenshot.js
+++ screenshot.js
@@ -1,10 +1,23 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    await page.goto('https://google.ca/');
-    await page.screenshot({ path: 'google.png' });
-    
-    await browser.close(); })();
+  // Open browser
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate to Google
+  await page.goto('https://www.google.com');
+
+  // Type "Hello World!" in the search input and press Enter
+  await page.type("*[name='q']", 'Hello World!');
+  await page.keyboard.press('Enter');
+
+  // Wait for search results to load
+  await page.waitForSelector('#search');
+
+  // Take a screenshot of the search results
+  await page.screenshot({ path: 'search_results.png' });
+
+  // Close browser
+  await browser.close();
+})();
